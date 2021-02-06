17768c9529bf4487c1b213528907b1132c45f15f
Andrew Nalundasan <andrewn488@gmail.com>
2021-02-05 18:18:04 -0800

    First commit getting started on Data Exploration Projects! Loaded libraries and getting folder structure setup.
    Remember to always:
    
    1)Look at the data
    2) Visualize what data should look like when finished
    3) Think of how to get data from Point A to Point B
    4) After EVERY step, look at data again to make sure it's doing what I think it's doing.

---
+++ 01_code/data_exploration_code.R
@@ -0,0 +1,10 @@
+# Author: Andrew Nalundasan
+# For: OMSBA 5300, Seattle University
+# Date: 2/19/2021
+# Data Exploration Project
+
+# load libraries: 
+library(tidyverse)
+library(data.table)
+library(jtools)
+library(vtable)
