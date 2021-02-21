1ade43476eab70466030cb2e43e503bb1867ca1f
Andrew Nalundasan <andrewn488@gmail.com>
2021-02-20 17:48:02 -0800

    Just get some plots running then write up an analysis. You can do this.

--- 01_code/data_exploration_code.R
+++ 01_code/data_exploration_code.R
@@ -71,51 +71,102 @@ id_sc_trends_merged <- merge(x = id_sc_merged, y = trends_data, by = 'schname', 
 # select data to work with, filter for BS, rename variables to something understandable
 # filter out Nulls and Privacy Suppressed median earnings and convert to numeric
 working_data <- id_sc_trends_merged %>% 
-  select('UNITID', 'OPEID', 'INSTNM', 'PREDDEG', 'keyword', 'monthorweek', 'keynum', 'index',
+  select('UNITID', 'OPEID', 'INSTNM', 'PREDDEG', 'keyword', 'monthorweek', 'keynum', 'index','CONTROL',
          'md_earn_wne_p10-REPORTED-EARNINGS') %>% 
-  rename(inst_name = INSTNM, pred_degree = PREDDEG, 
+  rename(inst_name = INSTNM, pred_degree = PREDDEG,
+         public_private = CONTROL,
          median_earnings = 'md_earn_wne_p10-REPORTED-EARNINGS') %>% 
   filter(pred_degree == 3) %>% 
   filter(median_earnings != 'NULL') %>% 
   filter(median_earnings != 'PrivacySuppressed') %>% 
   mutate(median_earnings = as.numeric(median_earnings))
 
+
 # median salary == 41800. This is the line dividing high earning vs. low earning
 median_earnings_threshold <- median(working_data$median_earnings)
+high_earning_uni <- median_earnings_threshold < working_data$median_earnings
+
 
 # standardize trends data Index by keynum:
-# 
+# add columns for high and low earning school compared to threshold
 standardized_trends_index <- working_data %>%
   group_by(keynum) %>%
   mutate(standardized_index = (index - mean(index, na.rm = TRUE)) / sd(index)) %>% 
-  summarize(inst_name, keyword, monthorweek, keynum, median_earnings, standardized_index)
+  mutate(high_earning_school = median_earnings > median_earnings_threshold)
+
+# select variables that I want to use in model
+tidy_data <- standardized_trends_index %>% 
+  select(inst_name, monthorweek, median_earnings, public_private,
+            high_earning_school, standardized_index)
 
 
 # get 'prior to Sept 2015' and 'post Sept 2015'
-pre_sept_2015 <- standardized_trends_index %>%
-  mutate(by_year = substr(monthorweek, 1, 10)) %>% 
-  mutate(by_year = as.Date(by_year)) %>% 
-  filter(by_year <= '2015-08-31')
+pre_sept_2015 <- tidy_data %>%
+  mutate(pre_sept_2015 = substr(monthorweek, 1, 10)) %>% 
+  mutate(pre_sept_2015 = as.Date(pre_sept_2015)) %>% 
+  filter(pre_sept_2015 <= '2015-08-31') 
 
-post_sept_2015 <- standardized_trends_index %>%
+post_sept_2015 <- tidy_data %>%
   mutate(by_year = substr(monthorweek, 1, 10)) %>% 
   mutate(by_year = as.Date(by_year)) %>% 
   filter(by_year > '2015-08-31')
 
+# run regressions of models for pre and post Sept 2015 and export
+m1 <- lm(median_earnings ~ standardized_index + high_earning_school + factor(public_private), data = pre_sept_2015)
+m2 <- lm(median_earnings ~ standardized_index + high_earning_school + factor(public_private), data = post_sept_2015)
+m3 <- lm(log(median_earnings) ~ standardized_index, data = pre_sept_2015)
+m4 <- lm(log(median_earnings) ~ standardized_index, data = post_sept_2015)
+
+export_summs(m1, m2, m3, m4)
 
 
-
- 
-  filter(by_month >= 2015-09)
-  group_by(by_month) %>% 
-  summarize(UNITID, OPEID, inst_name, keyword, median_earnings, standardized_trends)
-
-# should "start of September 2015" be accounted for in regression???
-  
+plot_coefs(m1, m2)
+effect_plot(m2, pred = 'standardized_index', plot.points = TRUE)
 
 
-  
-  
+# Geom Density
+ggplot(tidy_data, aes(x = standardized_index)) + 
+  geom_density() + 
+  labs(x = 'Standardized Index', y = 'Median Earnings', title = 'Distribution of DENSITY')
+
+# Geom Bar
+ggplot(tidy_data, aes(x = public_private)) + 
+  geom_bar() + 
+  labs(x = 'public_private', title = 'Bar Chart')
+
+# Geom Point
+ggplot(tidy_data, aes(x = median_earnings, y = standardized_index)) + 
+  geom_point() + 
+  labs(x = 'Median Earnings', y = 'Standardized Index', title = 'Distribution of POINT')
+
+ggplot(tidy_data, aes(x = standardized_index, y = median_earnings)) + 
+  geom_point() + 
+  labs(x = 'Median Earnings', y = 'Standardized Index', title = 'Distribution of POINT')
+
+
+# Geom Point/Smooth
+ggplot(standardized_trends_index, aes(x = public_private, y = median_earnings)) + 
+  geom_point() + 
+  geom_smooth() +
+  labs(x = 'Public Private', y = 'Median Earnings', title = 'Distribution of POINT/SMOOTH')
+
+# plot to a graph and check it out!
+ggplot(data = tidy_data) +
+  geom_histogram(mapping = aes(x = median_earnings), binwidth = 1000) + 
+  xlab('Median Earnings') + 
+  ggtitle('Distribution of Median Earnings')
+
+ggplot(data = standardized_trends_index, mapping = aes(x = fitted(median_earnings),
+                                                       y = rstandard(standardized_index))) +
+  geom_point() + 
+  geom_smooth() +
+  labs(x = 'Median Earnings', y = 'Standardized Index', title = 'PLOT DADDY')
+
+# Fitted vs. Residual Plot
+ggplot(data = profit_mlr1_01, mapping = aes(x = fitted(profit_mlr1_01), y = rstandard(profit_mlr1_01))) +
+  geom_point() +
+  geom_smooth() +
+  labs(x = "Residual", y = "Fitted", title = "Residual vs. Fitted - Profit by Household (Model 01)")
 
 
 
